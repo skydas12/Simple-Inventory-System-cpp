2f84915f3aa1cde9a84d6142095a92fcabb7a930
skydas12 <dovydas.stonys.2@gmail.com>
2026-04-30 22:47:34 +0300

    Not a lot was done today, but cleaned up main by moving reading/printing via functions only

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+struct product {
+    string name;
+    int quantity;
+    double price;
+};
+
+bool readProducts(vector<product> &products);
+void printProducts(const vector<product> &products);
 
 int main() {
-    std::cout << "Hello second project" << std::endl;
+
+    vector<product> products;
+
+    if (readProducts(products)) {
+        printProducts(products);
+    }
+    else {
+        cout << "Error reading input." << endl;
+    }
+
     return 0;
+}
+bool readProducts(vector<product> &products) {
+    ifstream input;
+    input.open("input.txt");
+
+    if (input.fail()) {
+        return false;
+    }
+        product temp;
+        while (input >> temp.name >> temp.quantity >> temp.price) {
+            products.push_back(temp);
+        }
+    input.close();
+    return true;
+}
+void printProducts(const vector<product> &products) {
+    ofstream output;
+    output.open("output.txt");
+    output << fixed << setprecision(2);
+    for (size_t i = 0; i < products.size(); i++) {
+        output << products[i].name << " "
+        << products[i].quantity << " "
+        << products[i].price << endl;
+    }
+    output.close();
 }
